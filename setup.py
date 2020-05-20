41ff4ee8e3beb3d08a094d0c5384eaacb940ddc8
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2020-05-20 08:26:53 +0100

    Make setup.py work from any working directory.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+import os
+
 from setuptools import setup, find_packages
 
-with open("vc2_conformance_data/version.py", "r") as f:
+version_file = os.path.join(
+    os.path.dirname(__file__), "vc2_conformance_data", "version.py",
+)
+with open(version_file, "r") as f:
     exec (f.read())  # noqa: E211
 
 setup(
