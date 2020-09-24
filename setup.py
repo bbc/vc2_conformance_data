e07e162889b2ee5496c7320df3aa3c6d937486fc
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2020-09-24 14:15:57 +0100

    Remove references to upcoming RP

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author="BBC R&D",
     description=(
         "Test data and images for the conformance testing software "
-        "for the SMPTE ST 2042-2 VC-2 professional video codec."
+        "for the SMPTE ST 2042-1 VC-2 professional video codec."
     ),
     license="GPLv2",
     classifiers=[
@@ -30,7 +30,7 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
-    keywords="smpte-RP-2042-3 vc2 dirac dirac-pro conformance data",
+    keywords="vc2 dirac dirac-pro conformance data",
     install_requires=[],
     entry_points={"console_scripts": []},
 )
